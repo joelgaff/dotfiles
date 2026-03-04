efc35aa792e8fc992b1939f068e0085fae54c0c1
Joel Gaff Jr <joelga@hey.com>
2026-03-04 11:19:23 -0800

    Replace nvim theme.lua symlink with Lua redirect for stow compatibility

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,6 @@
-/home/joel/.config/omarchy/current/theme/neovim.lua
+-- Load theme from omarchy's current theme
+local theme_path = vim.fn.expand("~/.config/omarchy/current/theme/neovim.lua")
+if vim.fn.filereadable(theme_path) == 1 then
+  return dofile(theme_path)
+end
+return {}
